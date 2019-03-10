5e16108376ec15871505f1162e80e27ef435b808
James LeClair <james@pixelpro.co>
2019-03-10 18:31:27 -0400

    simplify useRadio

--- src/hooks/useRadio.js
+++ src/hooks/useRadio.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react'
 
 export default function useRadio ({ input, events }) {
   const [isChecked, setIsChecked] = useState(input.props.defaultChecked || input.props.checked)
-  
+  const { name, value } = input.props
+
   function onClick(e) {
     setIsChecked(true)
-    events.emit('field:update', { name: input.props.name, value: input.props.value })
+    events.emit('field:update', { name, value })
     if (typeof input.props.onClick === 'function') {
       input.props.onClick(e)
     }
@@ -17,7 +18,6 @@ export default function useRadio ({ input, events }) {
   })
 
   function handleFieldUpdate (data) {
-    const { name, value } = input.props
     //short circuit if the field being updated is this one
     if (data.name === name && data.value === value) { return }
     // if another radio with the same name changes, set this as unchecked
@@ -25,7 +25,7 @@ export default function useRadio ({ input, events }) {
   }
 
   function getValue() {
-    return isChecked ? input.props.value : null
+    return isChecked ? value : null
   }
 
   return { getValue, onClick, checked: isChecked }
