b5066f04b55637e251fd16a6fa5b4ed98d534cbd
James LeClair <james@pixelpro.dev>
2020-06-02 17:08:43 -0400

    remove storybook and update dependencies

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react'
-
-const storyFiles = require.context('../src/', true, /\.storyfile\.js$/)
-
-function loadStories() {
-  storyFiles.keys().forEach((filename) => storyFiles(filename))
-}
-
-configure(loadStories, module)
